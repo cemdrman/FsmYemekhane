564b595e739384ec2fb3b25f7c7d6cb720efbaa1
cemdrman <cemdrman@gmail.com>
2017-10-29 03:42:30 +0300

    InfoActivity

---
+++ app/src/main/java/cemdirman/com/yemekhane/InfoActivity.java
@@ -0,0 +1,19 @@
+package cemdirman.com.yemekhane;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cem on 10/29/17.
+ */
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+    }
+}
